ce39dec54277c1551dcf4474277a1d1f6149ae88
devyuna <devyuna030@gmail.com>
2021-03-24 23:04:06 +0900

    make buttons

--- app/src/main/java/com/example/test_android/MainActivity.java
+++ app/src/main/java/com/example/test_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.test_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String str = intent.getExtras().getString("키");
+//        Log.d("로그", str);
+//        EditText editText = findViewById(R.id.edit_text);
+//        editText.setText(str);
+        ((EditText)findViewById(R.id.edit_text)).setText(str);
     }
 }
